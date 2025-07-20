f2b0cd2ca9779eebbd30797cd8068ba3fd4870a5
tejasri1209 <tejasripolu@gmail.com>
2025-07-20 23:37:07 +0530

    CarData.java

---
+++ CarData.java
@@ -0,0 +1,26 @@
+
+public class CarData {
+    public static void main(String[] args) {
+        String Car_Name = "Mahindra Thar";
+        float ARAI_Mileage = 15.2f;
+        String Fuel_Type = "Diesel";
+        int No_of_Cylinders = 4;
+        String Max_Power = "172bhp@35000rpm";
+        String Max_Torque = "370Nm@1500-3000rpm";
+        int Seating_Capacity = 5;
+        String Transmission_Type = "Automatic";
+        String Fuel_tank_Capacity = "57 litres";
+        String Body_Type = "SUV";
+
+        System.out.println(Car_Name);
+        System.out.println(ARAI_Mileage);
+        System.out.println(Fuel_Type);
+        System.out.println(No_of_Cylinders);
+        System.out.println(Max_Power);
+        System.out.println(Max_Torque);
+        System.out.println(Seating_Capacity);
+        System.out.println(Transmission_Type);
+        System.out.println(Fuel_tank_Capacity);
+        System.out.println(Body_Type );
+    }
+}
